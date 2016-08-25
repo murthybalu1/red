0f4f5d0ea84f452486279e0e9d2e637789d82a82
murthybalu1 <murthyusa01@gmail.com>
2016-08-25 17:22:12 +0000

    checkin

---
+++ git-uninstall/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: git-uninstall
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+package "git" do
+  action :uninstall
+end
